f1ff9635b0244ce646c79a6531a2e9499c5ecd51
Angel Cardenas <saikkopat@protonmail.com>
2025-06-20 14:17:56 -0600

    :tada: Inicializacion de la libreria.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+
+  <div class="text-red-400 titulo-1">
+    This is Cabin and not blue!
+    <p class="text-blue-100 cuerpo-1">
+      This should be Roboto.
+    </p>
+  </div>
+
+</template>
